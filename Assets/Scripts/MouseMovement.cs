e85846d5c7fd4b50df8e6a328b5da32f04416dbd
xxxcloa <bigguyjoe0@gmail.com>
2025-12-31 23:57:03 +1100

    added player controller n floor

---
+++ Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+    float xRotation = 0f;
+    float yRotation = 0f;
+    public float topClamp = -90f;
+    public float bottomClamp = 90f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
+        yRotation += mouseX;
+        
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+}
